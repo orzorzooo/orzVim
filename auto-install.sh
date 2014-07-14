fd59d87d5ee3bf9d4bb4c2d57c5b53abc1d41f88
orzorzooo <orzorzooo@gmail.com>
2014-07-14 18:23:40 +0800

    fix

--- auto-install.sh
+++ auto-install.sh
@@ -2,27 +2,10 @@
 #參考vgod改的
 VIMHOME=~/.vim
 
-warn() {
-	    echo "$1" 
-}
+if [ -e .vimrc  ]; then mv .vimrc .vimrc_bak; fi
+if [ -e .vim  ]; then mv .vim .vim_bak; fi
 
-die() {
-			warn "$1"
-			mkdir ~/原先vim備份
-			cp ~/.vimrc ~/原先vim備份/vimrc
-			rm ~/.vimrc
-}
-
-die2(){
-	    warn "$1"
-			cp -r $VIMHOME ~/原先vim備份/
-			rm -rf $VIMHOME
-}
-
-[ -e "~/.vimrc"  ] && die ".vimrc已經存在!我幫你COPY到家目錄/原先vim備份!"
-[ -e "$VIMHOME"  ] && die2 ".vim已經存在啦!!我幫你COPY到家目錄原先/vim備份!"
-
-git clone git://github.com/orzorzooo/orzVim.git "$VIMHOME"
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 cd ~
 ln -s $VIMHOME/vimrc ~/.vimrc
